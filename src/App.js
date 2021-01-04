340a8ab3ad1c023fe0685e2cf3ca1e18ec1526a2
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-01-04 21:42:08 +0900

    added check user sessions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import { checkUserSession } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 import Header from './components/Header/Header';
@@ -13,6 +14,10 @@ import { SignInAndSignUpPage } from './pages/SignInAndSignUpPage/SignInAndSignUp
 import './App.css';
 
 class App extends React.Component {
+  componentDidMount() {
+    const { checkUserSession } = this.props;
+    checkUserSession();
+  }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -37,4 +42,8 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 })
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
