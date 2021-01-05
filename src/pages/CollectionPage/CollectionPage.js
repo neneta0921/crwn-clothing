c496e7e8eadea4f0d63b360b60b4fe824babbb44
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-01-05 10:58:18 +0900

    using hooks CollectionPage for practice

--- src/pages/CollectionPage/CollectionPage.js
+++ src/pages/CollectionPage/CollectionPage.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { selectCollection } from '../../redux/shop/shopSelectors';
@@ -9,7 +10,19 @@ import {
   CollectionTitle
 } from './CollectionPageStyles'
 
+import { firestore } from '../../firebase/firebaseUtils';
+
 const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    console.log('I am subscribing');
+    const unsubscribeFromCollections = firestore
+      .collection('collection')
+      .onSnapshot(snapshot => console.log(snapshot))
+    return () => {
+      console.log('I am unsubscribe')
+      unsubscribeFromCollections();
+    }
+  }, [])
   const { title, items } = collection;
   return (
     <CollectionPageContainer>
