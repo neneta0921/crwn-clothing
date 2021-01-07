e86f4f93b0180d381c1e7c31b59891251c1f4ec4
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-01-07 17:39:58 +0900

    adding compression library for gzipping on heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -9,6 +10,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -16,22 +18,22 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  })
+  });
 }
 
-app.listen(port, error => {
-  if (error) throw error
+app.listen(port, (error) => {
+  if (error) throw error;
   console.log('Server running on port ' + port);
-})
+});
 
 app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: 'jpy'
-  }
+    currency: 'jpy',
+  };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
@@ -39,5 +41,5 @@ app.post('/payment', (req, res) => {
     } else {
       res.status(200).send({ success: stripeRes });
     }
-  })
-})
+  });
+});
