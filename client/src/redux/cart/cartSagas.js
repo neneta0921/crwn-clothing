31d2ec5b5143812d0ed7b88892610ec2b22511dc
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-01-09 09:19:55 +0900

    store cart item when user login

--- client/src/redux/cart/cartSagas.js
+++ client/src/redux/cart/cartSagas.js
@@ -1,16 +1,50 @@
-import { all, call, takeLatest, put } from 'redux-saga/effects';
+import { all, call, takeLatest, put, select } from 'redux-saga/effects';
 
+import { getUserCartRef } from '../../firebase/firebaseUtils';
 import { UserActionTypes } from '../user/userTypes';
-import { clearCart } from './cartActions';
+import { selectCurrentUser } from '../user/userSelectors';
+import { clearCart, setCartFromFirebase } from './cartActions';
+import { selectCartItems } from './cartSelectors';
+import { CartActionTypes } from './cartTypes';
 
 export function* clearCartOnSignOut() {
   yield put(clearCart());
+}
+
+export function* updateCartInFirebase() {
+  const currentUser = yield select(selectCurrentUser);
+  if (currentUser) {
+    try {
+      const cartRef = yield getUserCartRef(currentUser.id);
+      const cartItems = yield select(selectCartItems);
+      yield cartRef.update({ cartItems });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export function* checkCartFromFirebase({ payload: user }) {
+  const cartRef = yield getUserCartRef(user.id);
+  const cartSnapshot = yield cartRef.get();
+  yield put(setCartFromFirebase(cartSnapshot.data().cartItems));
 }
 
 export function* onSignOutSuccess() {
   yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut);
 }
 
+export function* onUserSignIn() {
+  yield takeLatest(UserActionTypes.SIGN_IN_SUCCESS, checkCartFromFirebase);
+}
+
+export function* onCartChange() {
+  yield takeLatest(
+    [CartActionTypes.ADD_ITEM, CartActionTypes.REMOVE_ITEM, CartActionTypes.CLEAR_ITEM_FROM_CART],
+    updateCartInFirebase
+  );
+}
+
 export function* cartSagas() {
-  yield all([call(onSignOutSuccess)]);
+  yield all([call(onSignOutSuccess), call(onCartChange), call(onUserSignIn)]);
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,10 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const configureRoutes = require('./routes');
-configureRoutes(app);
-
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
@@ -27,4 +26,20 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, (error) => {
   if (error) throw error;
   console.log('Server running on port ' + port);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'jpy',
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
